90cad03f2e5d67a8b26f7a0c038b4618addfdec5
Toppawar <pablo.divergency@gmail.com>
2021-08-12 22:27:55 +0200

    release: v0.1

---
+++ src/hooks/useDatabase.js
@@ -0,0 +1,81 @@
+import { useMemo, useState, useEffect, useRef } from "react";
+import useFirebase from "./useFirebase";
+import useUser from "./useUser";
+
+import isClientSide from "../utils/isClientSide";
+
+const useFirestoreQuery = (query) => {
+  const [docs, setDocs] = useState([]);
+
+  // Store current query in ref
+  const queryRef = useRef(query);
+
+  // Compare current query with the previous one
+  useEffect(() => {
+    // Use Firestore built-in 'isEqual' method
+    // to compare queries
+    if (!queryRef?.curent?.isEqual(query)) {
+      queryRef.current = query;
+    }
+  }, [query, queryRef]);
+
+  console.log(queryRef);
+
+  // Re-run data listener only if query has changed
+  useEffect(() => {
+    if (!queryRef.current) {
+      return null;
+    }
+
+    // Subscribe to query with onSnapshot
+    const unsubscribe = queryRef.current.onSnapshot((querySnapshot) => {
+      // Get all documents from collection - with IDs
+      const data = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(data);
+      // Update state
+      setDocs(data);
+    });
+
+    // Detach listener
+    return unsubscribe;
+  }, [queryRef]);
+
+  return docs;
+};
+
+const useDatabase = (ref) => {
+  const { user } = useUser();
+  const firebase = useFirebase();
+  const database = firebase.firestore();
+  const collectionByRef = useMemo(() => {
+    if (ref && isClientSide()) {
+      return database.collection(ref);
+    }
+    return undefined;
+  }, [ref]);
+
+  const data = useFirestoreQuery(
+    collectionByRef.orderBy("createdAt", "asc").limit(100)
+  );
+
+  const setData = (data) => {
+    collectionByRef.add({
+      text: data,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: user.uid,
+      displayName: user.username,
+      photoURL: user.avatar,
+    });
+  };
+
+  return {
+    data,
+    collectionByRef,
+    setData,
+  };
+};
+
+export default useDatabase;
